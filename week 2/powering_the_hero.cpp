952ee92c7f8fde024721caf07a1b51184f8ead81
Nazmul-Hassan10 <discord0nazmulhassan@gmail.com>
2024-11-10 00:16:05 +0600

    9 nov

---
+++ week 2/powering_the_hero.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        priority_queue<int> bonus;
+        int hero_total = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == 0) {
+                if (!bonus.empty()) {
+                    hero_total += bonus.top();
+                    bonus.pop();
+                }
+            } else {
+                bonus.push(v[i]);
+            }
+        }
+
+        cout << hero_total << endl;
+    }
+
+    return 0;
+}
