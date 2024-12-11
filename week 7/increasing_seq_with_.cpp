2cd91c84622a4b7a72e5104a538f600a6f5fb13d
Nazmul-Hassan10 <discord0nazmulhassan@gmail.com>
2024-12-11 23:57:05 +0600

    11 dec

---
+++ week 7/increasing_seq_with_.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        deque<long long> v;
+        for (int k = 0; k <= __lg(n); k++) {
+            if ((n >> k) & 1) {
+                long long value = n - (1LL << k);
+                if (value > 0) {
+                    v.push_front(value);
+                }
+            }
+        }
+
+        v.push_back(n);
+        cout << v.size() << endl;
+
+        for (auto value : v) {
+            cout << value << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
