4711666676f8ebffb151b36982b22faf910f963c
Nazmul-Hassan10 <discord0nazmulhassan@gmail.com>
2025-01-11 22:32:14 +0600

    11 jan

---
+++ week 10/interesting_drink.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+template <typename T>
+void debug(const vector<T>& v) {
+    for (const auto& x : v) cout << x << " ";
+    cout << endl;
+}
+
+template <typename T>
+T GCD(T a, T b) {
+    return __gcd(a, b);
+}
+
+template <typename T>
+T LCM(T a, T b) {
+    return (a / __gcd(a, b) * b);
+}
+
+template <typename T>
+bool isPrime(T n) {
+    if (n <= 1) return false;
+    for (T i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+/*kaede senpai*/
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto& x : v) cin >> x;
+
+    sort(all(v));
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int x;
+        cin >> x;
+
+        auto it = upper_bound(all(v), x);
+        cout << it - v.begin() << endl;
+    }
+
+    return 0;
+}
