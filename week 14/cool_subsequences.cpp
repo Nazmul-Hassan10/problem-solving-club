b515b3e90e3679e27010111f39738c720622e14d
Nazmul-Hassan10 <discord0nazmulhassan@gmail.com>
2025-02-15 22:23:47 +0600

    15 feb

---
+++ week 14/cool_subsequences.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+template <typename T>
+void debug(const vector<T>& v) {
+    for (const auto& x : v) cout << x << " ";
+    cout << endl;
+}
+
+template <typename T>
+T GCD(T a, T b) {
+    return __gcd(a, b);
+}
+
+template <typename T>
+T LCM(T a, T b) {
+    return (a / __gcd(a, b) * b);
+}
+
+template <typename T>
+bool isPrime(T n) {
+    if (n <= 1) return false;
+    for (T i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+/*kaede senpai*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    set<int> seen;
+
+    for (int num : v) {
+        if (seen.count(num)) {
+            cout << "2" << endl << num << " " << num << endl;
+            return;
+        }
+        seen.insert(num);
+    }
+
+    cout << "-1" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
