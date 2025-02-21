aeb861de213d987a81480e3d2429e14240a927cf
Nazmul-Hassan10 <discord0nazmulhassan@gmail.com>
2025-02-21 11:44:03 +0600

    21 feb

---
+++ practice/The_Legend_of_Freya_the_Frog.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+template <typename T>
+void debug(const vector<T>& v) {
+    for (const auto& x : v) cout << x << " ";
+    cout << endl;
+}
+
+template <typename T>
+T GCD(T a, T b) {
+    return __gcd(a, b);
+}
+
+template <typename T>
+T LCM(T a, T b) {
+    return (a / __gcd(a, b) * b);
+}
+
+template <typename T>
+bool isPrime(T n) {
+    if (n <= 1) return false;
+    for (T i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+/*kaede senpai*/
+void solve() {
+    long long x, y, k;
+    cin >> x >> y >> k;
+
+    long long val_1 = (x + k - 1) / k;
+    long long val_2 = (y + k - 1) / k;
+    // cout << val_1 << " " << val_2 << endl;
+
+    if (val_1 > val_2) {
+        cout << val_1 * 2 - 1 << endl;
+    } else {
+        cout << val_2 * 2 << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
